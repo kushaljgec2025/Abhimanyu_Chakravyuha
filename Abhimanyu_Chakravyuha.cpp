2f8d320e875f7fbf8686284e93748c651402656e
Kushal Ghosh <108950724+kushaljgec2025@users.noreply.github.com>
2024-08-30 13:00:49 +0530

    Update Abhimanyu_Chakravyuha.cpp

--- Abhimanyu_Chakravyuha.cpp
+++ Abhimanyu_Chakravyuha.cpp
@@ -78,3 +78,28 @@ int main()
 
     return 0;
 }
+/*
+Test Case 1 : 
+enemyPower=[1 2 5 2 1 3 5 8 8 2 1]
+power=10
+a=2
+b=2
+
+Input : 
+1 2 5 2 1 3 5 8 8 2 1
+10 2 2
+
+Output : Abhimanyu can cross the Chakrvyuha
+
+Test Case 2 : 
+enemyPower=[1 2 5 2 1 3 5 8 8 2 9]
+power=10
+a=2
+b=2
+
+Input : 
+1 2 5 2 1 3 5 8 8 2 9
+10 2 2
+
+Output : Abhimanyu cannot cross the Chakrvyuha
+*/
